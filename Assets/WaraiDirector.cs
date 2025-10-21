c24fa4b86d16c9224839ac7f60f1a2bfbc95d924
ume20s <ume20s@gmail.com>
2025-10-22 08:55:55 +0900

    タップして単発笑い

--- Assets/WaraiDirector.cs
+++ Assets/WaraiDirector.cs
@@ -2,30 +2,41 @@ using UnityEngine;
 
 public class WaraiDirector : MonoBehaviour
 {
+    // 変数もろもろ
+    static public bool isLaughing = false;                  // 笑い中
+
     // 音声関連
     AudioSource audioSource;
     public AudioClip vYukodesu;                             // ゆうこです！
     public AudioClip[] vWarai = new AudioClip[5];           // 笑い声３種
 
     // ゲームオブジェクト
-    GameObject[] waraiImage = new GameObject[3];            // おてつきグラフィック
+    GameObject waraiStay;            //
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // ゲームオブジェクトの取得
+        waraiStay = GameObject.Find("stay");
 
         // 音声コンポーネントの取得
         audioSource = GetComponent<AudioSource>();
 
         // 最初に元気よく「ゆうこです！」        
         audioSource.PlayOneShot(vYukodesu);
+
+        // 笑い中オフ
+        isLaughing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isLaughing)
+        {
+            audioSource.PlayOneShot(vWarai[0]);
+            isLaughing = false;
+        }
     }
 }
--- Assets/tapToStart.cs
+++ Assets/tapToStart.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class tapToStart : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void tapToLaugh()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // タップされたら笑いだす
+        WaraiDirector.isLaughing = true;
     }
 }
