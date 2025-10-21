2776b978e6d4d3e22d33803ee9c43de6ccf398e4
ume20s <ume20s@gmail.com>
2025-10-21 20:22:19 +0900

    タップイベントの準備

--- Assets/WaraiDirector.cs
+++ Assets/WaraiDirector.cs
@@ -4,14 +4,22 @@ public class WaraiDirector : MonoBehaviour
 {
     // 音声関連
     AudioSource audioSource;
-    public AudioClip vYukodesu;
-    public AudioClip[] vWarai = new AudioClip[5];
+    public AudioClip vYukodesu;                             // ゆうこです！
+    public AudioClip[] vWarai = new AudioClip[5];           // 笑い声３種
+
+    // ゲームオブジェクト
+    GameObject[] waraiImage = new GameObject[3];            // おてつきグラフィック
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // ゲームオブジェクトの取得
+
         // 音声コンポーネントの取得
         audioSource = GetComponent<AudioSource>();
+
+        // 最初に元気よく「ゆうこです！」        
         audioSource.PlayOneShot(vYukodesu);
     }
 
